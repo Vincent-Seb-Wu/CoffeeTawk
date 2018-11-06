aa6675c5c89fa3723c860e25014624b0f94bd861
Vincent Wu <vincentwu09@yahoo.com>
2018-11-06 19:02:54 +0800

    new

---
+++ vincentapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Student from `./Components/student`
+import Button from `./Components/button`
+
+class App extends Component {
+  render() {
+    return (
+
+
+    );
+  }
+}
+
+export default App;
